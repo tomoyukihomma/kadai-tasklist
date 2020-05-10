34dd8ea926c992ed12ee6b4daf63dadf2140af4f
tomo.h <mahon.tomo@gmail.com>
2020-05-10 13:46:25 +0000

    addsessions_controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,14 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
-
+  before_action :correct_user
+  
+  def show
+  end
+  
   def new
     @task = Task.new
   end
-  
+    
   def create
     @task = current_user.tasks.build(task_params)
     if @task.save
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,6 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
-  def index
-    @users = User.order(id: :desc)
-  end
-
-  def show
-    @user = User.find(params[:id])
-    @tasks = @user.tasks.order(id: :desc)
-    counts(@user)
-  end
-
+  before_action :require_user_logged_in, only: [:new, :create]
+  
   def new
     @user = User.new
   end
